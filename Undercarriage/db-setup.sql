0e1f7c1494a2b3303f812bac6972d612397b0e83
Larry Athey <panhandleponics@larryathey.com>
2023-07-18 13:06:00 -0600

    Create db-setup.sql

---
+++ Undercarriage/db-setup.sql
@@ -0,0 +1,7 @@
+DROP USER 'rssdbuser'@'localhost';
+DROP DATABASE rpismartstill;
+CREATE DATABASE rpismartstill;
+CREATE USER rssdbuser@localhost IDENTIFIED BY 'rssdbpasswd';
+USE rpismartstill;
+GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER, LOCK TABLES, EXECUTE, CREATE ROUTINE, ALTER ROUTINE, TRIGGER ON `ClimateCzar`.* TO 'rssdbuser'@'localhost';
+FLUSH PRIVILEGES;
