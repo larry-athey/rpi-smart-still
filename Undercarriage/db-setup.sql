af05f98783785c865576dbbc161296917bca9f22
Larry Athey <panhandleponics@larryathey.com>
2023-07-18 14:58:56 -0600

    Update db-setup.sql

--- Undercarriage/db-setup.sql
+++ Undercarriage/db-setup.sql
@@ -1,7 +1,7 @@
-DROP USER 'rssdbuser'@'localhost';
-DROP DATABASE rpismartstill;
-CREATE DATABASE rpismartstill;
+DROP USER IF EXISTS 'rssdbuser'@'localhost';
+DROP DATABASE IF EXISTS `rpismartstill`;
+CREATE DATABASE `rpismartstill`;
 CREATE USER rssdbuser@localhost IDENTIFIED BY 'rssdbpasswd';
-USE rpismartstill;
+USE `rpismartstill`;
 GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER, LOCK TABLES, EXECUTE, CREATE ROUTINE, ALTER ROUTINE, TRIGGER ON `rpismartstill`.* TO 'rssdbuser'@'localhost';
 FLUSH PRIVILEGES;
