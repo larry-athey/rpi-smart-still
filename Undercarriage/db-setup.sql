1b0eb0b6c621a10c3a911da9fe8585d751ff362a
Larry Athey <panhandleponics@larryathey.com>
2023-07-18 19:04:24 -0600

    Update db-setup.sql

--- Undercarriage/db-setup.sql
+++ Undercarriage/db-setup.sql
@@ -34,3 +34,24 @@ CREATE TABLE `output_table` (
 ALTER TABLE `output_table` ADD PRIMARY KEY (`ID`);
 
 ALTER TABLE `output_table` MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+CREATE TABLE `settings` (
+  `ID` int(11) NOT NULL,
+  `boiler_addr` varchar(24) DEFAULT NULL,
+  `dephleg_addr` varchar(24) DEFAULT NULL,
+  `column_addr` varchar(24) DEFAULT NULL,
+  `valve1_total` int(11) DEFAULT NULL,
+  `valve1_pulse` int(11) DEFAULT NULL,
+  `valve1_current` int(11) DEFAULT NULL,
+  `valve2_total` int(11) DEFAULT NULL,
+  `valve2_pulse` int(11) DEFAULT NULL,
+  `valve2_current` int(11) DEFAULT NULL,
+  `heating_total` int(11) DEFAULT NULL,
+  `heating_pulse` int(11) DEFAULT NULL,
+  `heating_current` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+ALTER TABLE `settings` ADD PRIMARY KEY (`ID`);
+
+ALTER TABLE `settings` MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
