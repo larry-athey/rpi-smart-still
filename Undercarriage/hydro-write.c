3c329c0adda70cb8bb3c450119f5e05f195cff9f
Larry Athey <panhandleponics@larryathey.com>
2023-08-01 08:52:14 -0600

    Create hydro-write.c

---
+++ Undercarriage/hydro-write.c
@@ -0,0 +1,35 @@
+//------------------------------------------------------------------------------------------------
+// RPI-Smart-Still Hydrometer Serial Data Reader v1.0.1 released July 14, 2023
+//
+// Simple program to send commands to the hydrometer to reboot it or recalibrate the load cell.
+//------------------------------------------------------------------------------------------------
+#include "stdio.h"
+#include "string.h"
+#include "errno.h"
+#include "wiringPi.h"
+#include "wiringSerial.h"
+//------------------------------------------------------------------------------------------------
+int main(int argc,char **argv) {
+  int UART;
+
+  if (argc != 2) {
+    fprintf(stderr,"Invalid usage, requires a 1 character command to be passed");
+    return 1;
+  }
+
+  if ((UART = serialOpen("/dev/ttyAMA0",9600)) < 0) {
+    fprintf(stderr,"Unable to open serial device: %s\n",strerror(errno));
+    return 1;
+  }
+
+  if (wiringPiSetup () == -1) {
+    fprintf(stdout,"Unable to start wiringPi: %s\n",strerror(errno));
+    return 1;
+  }
+
+  serialPuts(UART,argv[1]);
+  delay(500);
+  serialFlush(UART);
+
+}
+//------------------------------------------------------------------------------------------------
