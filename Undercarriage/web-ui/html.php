7c00097d9bbf8cc8140eeefcc3afe6e8cb8759e9
Larry Athey <panhandleponics@larryathey.com>
2023-08-03 21:06:18 -0600

    Update html.php

--- Undercarriage/web-ui/html.php
+++ Undercarriage/web-ui/html.php
@@ -2,8 +2,12 @@
 //---------------------------------------------------------------------------------------------------
 require_once("subs.php");
 //---------------------------------------------------------------------------------------------------
-function rss_menu_bar() {
-  //$DBcnx  = mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,"ClimateCzar");
+function DrawMenu($DBcnx) {
+  $Result   = mysqli_query($DBcnx,"SELECT * FROM settings WHERE ID=1");
+  $Settings = mysqli_fetch_assoc($Result);
+  $Result   = mysqli_query($DBcnx,"SELECT * FROM programs WHERE ID=" . $Settings["active_program"]);
+  $Program  = mysqli_fetch_assoc($Result);
+
   $Content  = "<nav class=\"navbar navbar-expand-lg navbar-dark bg-dark\">";
   $Content .=   "<div class=\"container-fluid\">";
   $Content .=     "<a class=\"navbar-brand\" href=\"/\"><span class=\"iconify text-white\" style=\"font-size: 1.5em;\" data-icon=\"logos:raspberry-pi\"></span>&nbsp;<span class=\"text-white\" style=\"font-weight: bold;\">RPi Smart Still</span></a>";
@@ -16,13 +20,13 @@ function rss_menu_bar() {
   $Content .=           "<a class=\"nav-link\" aria-current=\"page\" href=\"/\">Home</a>";
   $Content .=         "</li>";
   $Content .=         "<li class=\"nav-item\">";
-  $Content .=           "<a class=\"nav-link\" href=\"/?page=timeline\">Timeline</a>";
+  $Content .=           "<a class=\"nav-link\" href=\"?page=timeline\">Timeline</a>";
   $Content .=         "</li>";
   $Content .=         "<li class=\"nav-item dropdown\">";
   $Content .=           "<a class=\"nav-link dropdown-toggle\" href=\"#\" role=\"button\" data-bs-toggle=\"dropdown\" aria-expanded=\"false\">Programs</a>";
   $Content .=           "<ul class=\"dropdown-menu\" style=\"background-color: #212529;\">";
 /*
-  $Result = mysqli_query($DBcnx,"SELECT * FROM DeviceGroups ORDER BY Name");
+  $Result = mysqli_query($DBcnx,"SELECT * FROM programs ORDER BY program_name");
   while ($Group = mysqli_fetch_assoc($Result)) {
     if ($Group["ID"] == CZ_GROUP) $GroupName =$Group["Name"];
     $Group["Name"] = str_replace(" ","&nbsp;",$Group["Name"]);
@@ -34,19 +38,19 @@ function rss_menu_bar() {
   $Content .=         "<li class=\"nav-item dropdown\">";
   $Content .=           "<a class=\"nav-link dropdown-toggle\" href=\"#\" role=\"button\" data-bs-toggle=\"dropdown\" aria-expanded=\"false\">Management</a>";
   $Content .=           "<ul class=\"dropdown-menu\" style=\"background-color: #212529;\">";
-  $Content .=             "<li><a class=\"dropdown-item\" href=\"/?page=programs\">Edit&nbsp;Programs</a></li>";
-  $Content .=             "<li><a class=\"dropdown-item\" href=\"/?page=valves\">Calibrate&nbsp;Valves</a></li>";
-  $Content .=             "<li><a class=\"dropdown-item\" href=\"/?page=sensors\">Configure&nbsp;Sensors</a></li>";
-  $Content .=             "<li><a class=\"dropdown-item disabled\" href=\"/?page=heating\"><span class=\"text-secondary\">Configure&nbsp;Heating</span></a></li>";
-  $Content .=             "<li><a class=\"dropdown-item\" href=\"/?page=hydrometer\">Calibrate&nbsp;Hydrometer</a></li>";
+  $Content .=             "<li><a class=\"dropdown-item\" href=\"?page=programs\">Edit&nbsp;Programs</a></li>";
+  $Content .=             "<li><a class=\"dropdown-item\" href=\"?page=valves\">Calibrate&nbsp;Valves</a></li>";
+  $Content .=             "<li><a class=\"dropdown-item\" href=\"?page=sensors\">Configure&nbsp;Sensors</a></li>";
+  $Content .=             "<li><a class=\"dropdown-item disabled\" href=\"?page=heating\"><span class=\"text-secondary\">Configure&nbsp;Heating</span></a></li>";
+  $Content .=             "<li><a class=\"dropdown-item\" href=\"?page=hydrometer\">Calibrate&nbsp;Hydrometer</a></li>";
   $Content .=             "<li><hr class=\"dropdown-divider\"></li>";
-  $Content .=             "<li><a class=\"dropdown-item\" href=\"/?run=1\">Start&nbsp;Run</a></li>";
-  $Content .=             "<li><a class=\"dropdown-item\" href=\"/?run=10\">Pause&nbsp;Run</a></li>";
-  $Content .=             "<li><a class=\"dropdown-item\" href=\"/?run=0\">Stop&nbsp;Run</a></li>";
+  $Content .=             "<li><a class=\"dropdown-item\" href=\"?run=1\">Start&nbsp;Run</a></li>";
+  $Content .=             "<li><a class=\"dropdown-item\" href=\"?run=10\">Pause&nbsp;Run</a></li>";
+  $Content .=             "<li><a class=\"dropdown-item\" href=\"?run=0\">Stop&nbsp;Run</a></li>";
   $Content .=           "</ul>";
   $Content .=         "</li>";
   $Content .=         "<li class=\"nav-item\">";
-  $Content .=           "<a class=\"nav-link disabled\">Current Program: Maximum Reflux</a>";
+  $Content .=           "<a class=\"nav-link disabled\">Current Program: " . $Program["program_name"] . "</a>";
   $Content .=         "</li>";
   $Content .=       "</ul>";
   $Content .=     "</div>";
@@ -71,5 +75,33 @@ function rss_menu_bar() {
         $Content .= "</div>";
       }
 */
+//---------------------------------------------------------------------------------------------------
+function ShowTemperatures($DBcnx) {
+  $Result   = mysqli_query($DBcnx,"SELECT * FROM settings WHERE ID=1");
+  $Settings = mysqli_fetch_assoc($Result);
+
+  $Content  = "<table class=\"table table-sm table-borderless\">";
+  $Content .=   "<tr><td>Dephleg&nbsp;Temperature:</td><td align=\"right\">" . FormatTemp($Settings["dephleg_temp"]) . "</td></tr>";
+  $Content .=   "<tr><td>Column&nbsp;Temperature:</td><td align=\"right\">" . FormatTemp($Settings["column_temp"]) . "</td></tr>";
+  $Content .=   "<tr><td>Boiler&nbsp;Temperature:</td><td align=\"right\">" . FormatTemp($Settings["boiler_temp"]) . "</td></tr>";
+  $Content .=   "<tr><td>Distillate&nbsp;Temperature:</td><td align=\"right\">" . FormatTemp($Settings["distillate_temp"]) . "</td></tr>";
+  $Content .= "</table>";
+  return $Content;
+}
+//---------------------------------------------------------------------------------------------------
+function DrawCard($DBcnx,$Body) {
+  $RandID   = "card_" . generateRandomString();
+  $Content  = "<div class=\"card\" style=\"width: 26em; margin-top: 0.5em;  margin-bottom: 0.5em; margin-left: 0.5em; margin-right: 0.5em;\">";
+  $Content .=   "<div class=\"card-body\">";
+  $Content .=     AjaxRefreshJS($Body,$RandID);
+  $Content .=     "<div id=\"$RandID\">";
+  if ($Body == "temperatures") {
+    $Content .= ShowTemperatures($DBcnx);
+  }
+  $Content .=     "</div>";
+  $Content .=   "</div>";
+  $Content .= "</div>";
+  return $Content;
+}
 //---------------------------------------------------------------------------------------------------
 ?>
