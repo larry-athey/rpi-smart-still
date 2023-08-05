7b4e1ef7616db74c551cea8ebbdcd919bcf23fdd
Larry Athey <panhandleponics@larryathey.com>
2023-08-05 15:42:38 -0600

    Update index.php

--- Undercarriage/web-ui/index.php
+++ Undercarriage/web-ui/index.php
@@ -63,7 +63,8 @@ $Content .=   "<div class=\"row\">";
 
 if (! isset($_GET["page"])) {
   $Content .= DrawCard($DBcnx,"temperatures",true);
-
+  $Content .= DrawCard($DBcnx,"program_temps",false);
+  $Content .= DrawCard($DBcnx,"valve_positions",true);
 } else {
   if ($_GET["page"] == "heating") {
 
