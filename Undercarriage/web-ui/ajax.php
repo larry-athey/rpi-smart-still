b5d34a40c4295ab55a63055b5c88cfaf8586f083
Larry Athey <panhandleponics@larryathey.com>
2023-08-05 18:48:51 -0600

    Update ajax.php

--- Undercarriage/web-ui/ajax.php
+++ Undercarriage/web-ui/ajax.php
@@ -4,7 +4,9 @@ require_once("html.php");
 //---------------------------------------------------------------------------------------------------
 $DBcnx = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
 
-if ($_GET["ID"] == "temperatures") {
+if ($_GET["ID"] == "hydrometer") {
+  $Content = ShowHydrometer($DBcnx);
+} elseif ($_GET["ID"] == "temperatures") {
   $Content = ShowTemperatures($DBcnx);
 } elseif ($_GET["ID"] == "valve_positions") {
   $Content = ShowValves($DBcnx);
