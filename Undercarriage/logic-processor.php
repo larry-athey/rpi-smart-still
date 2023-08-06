48e8744e803ff185bff8457ad07cc68caa367ce0
Larry Athey <panhandleponics@larryathey.com>
2023-08-06 17:44:30 -0600

    Update logic-processor.php

--- Undercarriage/logic-processor.php
+++ Undercarriage/logic-processor.php
@@ -6,7 +6,10 @@ require_once("/var/www/html/subs.php");
 set_time_limit(600);
 $DBcnx = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
 //---------------------------------------------------------------------------------------------
-
+$Result   = mysqli_query($DBcnx,"SELECT * FROM settings WHERE ID=1");
+$Settings = mysqli_fetch_assoc($Result);
+$Result   = mysqli_query($DBcnx,"SELECT * FROM programs WHERE ID=" . $Settings["active_program"]);
+$Program  = mysqli_fetch_assoc($Result);
 
 //---------------------------------------------------------------------------------------------
 mysqli_close($DBcnx);
