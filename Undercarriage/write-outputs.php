14f6cd87bff9d2cb7b2f140e381951edc6891c7d
Larry Athey <panhandleponics@larryathey.com>
2023-08-06 12:22:36 -0600

    Update write-outputs.php

--- Undercarriage/write-outputs.php
+++ Undercarriage/write-outputs.php
@@ -2,11 +2,76 @@
 <?php
 //---------------------------------------------------------------------------------------------
 require_once("/var/www/html/subs.php");
+require_once("voice-prompts.php");
 //---------------------------------------------------------------------------------------------
 set_time_limit(600);
 $DBcnx = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
 //---------------------------------------------------------------------------------------------
+$Result   = mysqli_query($DBcnx,"SELECT * FROM settings WHERE ID=1");
+$Settings = mysqli_fetch_assoc($Result);
 
+$Result   = mysqli_query($DBcnx,"SELECT * FROM output_table WHERE executed=0");
+while ($RS = mysqli_fetch_assoc($Result)) {
+  print_r($RS);
+  if ($RS["valve_id"] < 3) {
+    if ($RS["direction"] == 0) {
+      $Direction = "close";
+    } else {
+      $Direction = "open";
+    }
+    if ($Settings["speech_enabled"] == 1) {
+      if ($RS["valve_id"] == 1) {
+        if ($RS["direction"] == 0) {
+          SpeakMessage(1);
+        } else {
+          SpeakMessage(0);
+        }
+      } else {
+        if ($RS["direction"] == 0) {
+          SpeakMessage(3);
+        } else {
+          SpeakMessage(2);
+        }
+      }
+    }
+    shell_exec("/usr/share/rpi-smart-still/valve " . $RS["valve_id"] . " $Direction " . $RS["duration"]);
+    $Update = mysqli_query($DBcnx,"UPDATE output_table SET executed='1' WHERE ID=" . $RS["ID"]);
+    $Status = trim(shell_exec("/usr/share/rpi-smart-still/valve " . $RS["valve_id"] . " status"));
+    if ($Status == 0) {
+      $Update = mysqli_query($DBcnx,"UPDATE settings SET valve" . $RS["valve_id"] . "_position ='0' WHERE ID=1");
+    } elseif ($Status == 1) {
+      if ($RS["valve_id"] == 1) {
+        $Total = $Settings["valve1_total"];
+      } else {
+        $Total = $Settings["valve2_total"];
+      }
+      $Update = mysqli_query($DBcnx,"UPDATE settings SET valve" . $RS["valve_id"] . "_position ='$Total' WHERE ID=1");
+    }
+  } else {
+    if ($Settings["heating_polarity"] == 0) {
+      if ($RS["direction"] == 0) {
+        $Direction = "ccw";
+      } else {
+        $Direction = "cw";
+      }
+    } else {
+      if ($RS["direction"] == 0) {
+        $Direction = "cw";
+      } else {
+        $Direction = "ccw";
+      }
+    }
+    if ($Settings["speech_enabled"] == 1) {
+      if ($RS["direction"] == 0) {
+        SpeakMessage(5);
+      } else {
+        SpeakMessage(4);
+      }
+    }
+    shell_exec("/usr/share/rpi-smart-still/heating $Direction " . $RS["duration"]);
+    $Update = mysqli_query($DBcnx,"UPDATE output_table SET executed='1' WHERE ID=" . $RS["ID"]);
+  }
+}
 
 //---------------------------------------------------------------------------------------------
 mysqli_close($DBcnx);
