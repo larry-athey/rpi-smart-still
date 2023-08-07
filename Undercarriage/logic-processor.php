a5a99466009a981c89d82d10468bb6815051b3eb
Larry Athey <panhandleponics@larryathey.com>
2023-08-07 13:40:55 -0600

    Update logic-processor.php

--- Undercarriage/logic-processor.php
+++ Undercarriage/logic-processor.php
@@ -48,7 +48,21 @@ if (mysqli_num_rows($Result) > 0) {
     // Handle the active run
     if ($Logic["boiler_done"] == 0) {
       // Don't bother managing anything else until the boiler is up to temperature
-
+      if ($Settings["boiler_temp"] >= $Program["boiler_temp_low"]) {
+        if ($Settings["heating_enabled"] == 1) {
+          if ($Settings["speech_enabled"] == 1) SpeakMessage(10);
+          $Result  = mysqli_query($DBcnx,"SELECT * FROM heating_translation WHERE percent=70");
+          $Heating = mysqli_fetch_assoc($Result);
+          $Difference = $Settings["heating_position"] - $Heating["position"];
+          $Update = mysqli_query($DBcnx,"UPDATE settings SET heating_position='" . $Heating["position"] . "' WHERE ID=1");
+          $Update = mysqli_query($DBcnx,"UPDATE logic_tracker SET boiler_done='1',boiler_done_time=now(),boiler_last_adjustment=now(),boiler_last_direction='0'," .
+                                        "boiler_last_duration='$Difference',boiler_note='Boiler has reached minimum operating temperature, reducing heat to 70%' WHERE ID=1");
+          $Insert = mysqli_query($DBcnx,"INSERT INTO output_table (timestamp,auto_manual,valve_id,direction,duration,position,executed) " .
+                                        "VALUES (now(),'0','3','0','$Difference','" . $Heating["position"] . "','0')");
+        } else {
+          if ($Settings["speech_enabled"] == 1) SpeakMessage(11);
+        }
+      }
     } else {
 
     }
