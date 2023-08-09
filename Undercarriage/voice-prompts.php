803244e0d4cfacc3a0602ab6d9a7083156d65140
Larry Athey <panhandleponics@larryathey.com>
2023-08-09 10:35:53 -0600

    Update voice-prompts.php

--- Undercarriage/voice-prompts.php
+++ Undercarriage/voice-prompts.php
@@ -38,6 +38,8 @@ function SpeakMessage($ID) {
   $Msg[19]  = "Column is over temperature. Please decrease your heat a notch or two";
   $Msg[20]  = "Column is under temperature. Increasing heat to " . $Settings["heating_position"] . " steps";
   $Msg[21]  = "Column is over temperature. Decreasing heat to " . $Settings["heating_position"] . " steps";
+  $Msg[22]  = "Dephlegmator is under temperature. Decreasing cooling water flow";
+  $Msg[23]  = "Dephlegmator is over temperature. Increasing cooling water flow";
 
   shell_exec("/usr/bin/espeak -v english-us -s 160 \"$Msg[$ID]\" 2> /dev/null &");
   mysqli_close($DBcnx);
