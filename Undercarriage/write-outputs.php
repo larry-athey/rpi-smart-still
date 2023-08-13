163283b7526168b97c67fa512968188cf2bff53f
Larry Athey <panhandleponics@larryathey.com>
2023-08-12 18:57:02 -0600

    Update write-outputs.php

--- Undercarriage/write-outputs.php
+++ Undercarriage/write-outputs.php
@@ -7,12 +7,14 @@ require_once("voice-prompts.php");
 set_time_limit(600);
 $DBcnx = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
 //---------------------------------------------------------------------------------------------
+$Counter  = 0;
 $Result   = mysqli_query($DBcnx,"SELECT * FROM settings WHERE ID=1");
 $Settings = mysqli_fetch_assoc($Result);
 
 $Result   = mysqli_query($DBcnx,"SELECT * FROM output_table WHERE executed=0");
 while ($RS = mysqli_fetch_assoc($Result)) {
   print_r($RS);
+  $Counter ++;
   if ($RS["valve_id"] < 3) {
     // Control commands for the condenser and dephleg cooling valves
     if ($RS["direction"] == 0) {
@@ -37,18 +39,6 @@ while ($RS = mysqli_fetch_assoc($Result)) {
     }
     shell_exec("/usr/share/rpi-smart-still/valve " . $RS["valve_id"] . " $Direction " . $RS["duration"]);
     $Update = mysqli_query($DBcnx,"UPDATE output_table SET timestamp=now(),executed='1' WHERE ID=" . $RS["ID"]);
-    sleep(1);
-    $Status = trim(shell_exec("/usr/share/rpi-smart-still/valve " . $RS["valve_id"] . " status"));
-    if ($Status == 0) {
-      $Update = mysqli_query($DBcnx,"UPDATE settings SET valve" . $RS["valve_id"] . "_position ='0' WHERE ID=1");
-    } elseif ($Status == 1) {
-      if ($RS["valve_id"] == 1) {
-        $Total = $Settings["valve1_total"];
-      } else {
-        $Total = $Settings["valve2_total"];
-      }
-      $Update = mysqli_query($DBcnx,"UPDATE settings SET valve" . $RS["valve_id"] . "_position ='$Total' WHERE ID=1");
-    }
   } elseif ($RS["valve_id"] == 3) {
     // Control commands for the heating stepper motor
     if ($Settings["heating_polarity"] == 0) {
@@ -106,7 +96,22 @@ while ($RS = mysqli_fetch_assoc($Result)) {
     // Control commands to recalibrate the hydrometer
   }
 }
-
+//---------------------------------------------------------------------------------------------
+if ($Counter == 0) {
+  for ($x = 1; $x <= 2; $x ++) {
+    $Status = trim(shell_exec("/usr/share/rpi-smart-still/valve $x status"));
+    if ($Status == 0) {
+      $Update = mysqli_query($DBcnx,"UPDATE settings SET valve" . $x . "_position ='0' WHERE ID=1");
+    } elseif ($Status == 1) {
+      if ($x == 1) {
+        $Total = $Settings["valve1_total"];
+      } else {
+        $Total = $Settings["valve2_total"];
+      }
+      $Update = mysqli_query($DBcnx,"UPDATE settings SET valve" . $x . "_position ='$Total' WHERE ID=1");
+    }
+  }
+}
 //---------------------------------------------------------------------------------------------
 mysqli_close($DBcnx);
 //---------------------------------------------------------------------------------------------
