4f496994f5c57ffe292775515b8603c0bc6ea35b
Larry Athey <panhandleponics@larryathey.com>
2023-08-14 12:31:03 -0600

    Update process.php

--- Undercarriage/web-ui/process.php
+++ Undercarriage/web-ui/process.php
@@ -42,6 +42,42 @@ elseif (isset($_GET["calibrate_valves"])) {
   $Insert = mysqli_query($DBcnx,"INSERT INTO output_table (timestamp,auto_manual,valve_id,muted,executed) VALUES (now(),'1','4','0','0')");
 }
 //---------------------------------------------------------------------------------------------------
+elseif (isset($_GET["heat_jump"])) {
+  $Result   = mysqli_query($DBcnx,"SELECT * FROM settings WHERE ID=1");
+  $Settings = mysqli_fetch_assoc($Result);
+  $Result   = mysqli_query($DBcnx,"SELECT * FROM heating_translation WHERE percent=" . $_GET["value"]);
+  $Heating  = mysqli_fetch_assoc($Result);
+
+  if ($_GET["value"] == 0) {
+    $Heating["position"] = 0;
+  } elseif ($_GET["value"] == 100) {
+    $Heating["position"] = $Settings["heating_total"];
+  }
+
+  // Requires a difference in heating stepper position to process
+  $Difference = 0;
+  if ($Settings["heating_enabled"] == 1) {
+    if ($Heating["position"] > $Settings["heating_position"]) {
+      $Difference = $Heating["position"] - $Settings["heating_position"];
+      $Direction = 1;
+    } elseif ($Heating["position"] < $Settings["heating_position"]) {
+      $Difference = $Settings["heating_position"] - $Heating["position"];
+      $Direction = 0;
+    }
+    if ($Difference > 0) {
+      $Update = mysqli_query($DBcnx,"UPDATE settings SET heating_position='" . $Heating["position"] . "' WHERE ID=1");
+      if ($Settings["heating_analog"] == 1) { // A digital voltmeter doesn't mean that it's a digital voltage controller!
+        $Insert = mysqli_query($DBcnx,"INSERT INTO output_table (timestamp,auto_manual,valve_id,direction,duration,position,muted,executed) " .
+                                      "VALUES (now(),'0','3','0','" . $Settings["heating_position"] . "','0','1','0')," .
+                                             "(now(),'1','3','1','" . $Heating["position"] . "','" . $Heating["position"] . "','1','0')");
+      } else { // Digital voltage controllers and gas valves can just be adjusted up and down as necessary
+        $Insert = mysqli_query($DBcnx,"INSERT INTO output_table (timestamp,auto_manual,valve_id,direction,duration,position,muted,executed) " .
+                                      "VALUES (now(),'1','3','$Direction','$Difference','" . $Heating["position"] . "','1','0')");
+      }
+    }
+  }
+}
+//---------------------------------------------------------------------------------------------------
 elseif (isset($_POST["rss_edit_servos"])) {
   $Result   = mysqli_query($DBcnx,"SELECT * FROM settings WHERE ID=1");
   $Settings = mysqli_fetch_assoc($Result);
