9e7c46b6c4cf1221b2f0234d98e75bbd9f41876d
Larry Athey <panhandleponics@larryathey.com>
2023-08-16 18:20:36 -0600

    Update logic-processor.php

--- Undercarriage/logic-processor.php
+++ Undercarriage/logic-processor.php
@@ -335,7 +335,7 @@ if (mysqli_num_rows($Result) > 0) {
               $Update = mysqli_query($DBcnx,"UPDATE settings SET active_run='0',run_end=now() WHERE ID=1");
             }
           } else {
-            // In reflux mode, we dynamically adjust the program's column upper and lower temperature limits
+            // In reflux mode, we dynamically adjust the program's dephleg upper and lower temperature limits
           }
         }
       }
