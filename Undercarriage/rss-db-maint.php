9537ead8ebf55b2c31362c308277621da922915c
Larry Athey <panhandleponics@larryathey.com>
2023-08-19 22:08:26 -0600

    Create rss-db-maint.php

---
+++ Undercarriage/rss-db-maint.php
@@ -0,0 +1,17 @@
+#!/usr/bin/php
+<?php
+//---------------------------------------------------------------------------------------------
+require_once("/var/www/html/subs.php");
+//---------------------------------------------------------------------------------------------
+$DBcnx = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+//---------------------------------------------------------------------------------------------
+$Result = mysqli_query($DBcnx,"OPTIMIZE TABLE heating_translation");
+$Result = mysqli_query($DBcnx,"OPTIMIZE TABLE input_table");
+$Result = mysqli_query($DBcnx,"OPTIMIZE TABLE logic_tracker");
+$Result = mysqli_query($DBcnx,"OPTIMIZE TABLE output_table");
+$Result = mysqli_query($DBcnx,"OPTIMIZE TABLE programs");
+$Result = mysqli_query($DBcnx,"OPTIMIZE TABLE settings");
+//---------------------------------------------------------------------------------------------
+mysqli_close($DBcnx);
+//---------------------------------------------------------------------------------------------
+?>
