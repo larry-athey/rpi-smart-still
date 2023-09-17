2f2182427b4b319df601e06604a558dfd29870a3
Larry Athey <panhandleponics@larryathey.com>
2023-09-17 13:28:39 -0600

    Update rss-db-maint.php

--- Undercarriage/rss-db-maint.php
+++ Undercarriage/rss-db-maint.php
@@ -11,12 +11,6 @@ $Result = mysqli_query($DBcnx,"OPTIMIZE TABLE logic_tracker");
 $Result = mysqli_query($DBcnx,"OPTIMIZE TABLE output_table");
 $Result = mysqli_query($DBcnx,"OPTIMIZE TABLE programs");
 $Result = mysqli_query($DBcnx,"OPTIMIZE TABLE settings");
-
-$Result = mysqli_query($DBcnx,"SELECT * FROM voice_prompts WHERE timestamp < (NOW() - INTERVAL 5 MINUTE)");
-while ($RS = mysqli_fetch_assoc($Result)) {
-  unlink("/var/www/html/voice_prompts/" . $RS["filename"]);
-}
-$Result = mysqli_query($DBcnx,"DELETE FROM voice_prompts WHERE timestamp < (NOW() - INTERVAL 5 MINUTE)");
 $Result = mysqli_query($DBcnx,"OPTIMIZE TABLE voice_prompts");
 //---------------------------------------------------------------------------------------------
 mysqli_close($DBcnx);
