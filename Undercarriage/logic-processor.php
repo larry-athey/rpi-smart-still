5675aff49e80dc769e8ac46ae0a170b98b505239
Larry Athey <panhandleponics@larryathey.com>
2023-09-17 13:30:05 -0600

    Update logic-processor.php

--- Undercarriage/logic-processor.php
+++ Undercarriage/logic-processor.php
@@ -380,6 +380,13 @@ if (mysqli_num_rows($Result) > 0) {
   }
 }
 //---------------------------------------------------------------------------------------------
+// Clean up voice_prompts table and any MP3 files more than 5 minutes old
+$Result = mysqli_query($DBcnx,"SELECT * FROM voice_prompts WHERE timestamp < (NOW() - INTERVAL 5 MINUTE)");
+while ($RS = mysqli_fetch_assoc($Result)) {
+  unlink("/var/www/html/voice_prompts/" . $RS["filename"]);
+}
+$Result = mysqli_query($DBcnx,"DELETE FROM voice_prompts WHERE timestamp < (NOW() - INTERVAL 5 MINUTE)");
+//---------------------------------------------------------------------------------------------
 mysqli_close($DBcnx);
 //---------------------------------------------------------------------------------------------
 ?>
