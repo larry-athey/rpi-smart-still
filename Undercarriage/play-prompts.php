a14cc1c366dcbd3d5c0aaf51b2b758a90b8e1f12
Larry Athey <panhandleponics@larryathey.com>
2023-09-17 22:38:33 -0600

    Update play-prompts.php

--- Undercarriage/play-prompts.php
+++ Undercarriage/play-prompts.php
@@ -18,7 +18,7 @@ if ($Settings["speech_enabled"] == 1) {
   $Result = mysqli_query($DBcnx,"SELECT * FROM voice_prompts WHERE seen_by NOT LIKE '%localhost%' ORDER BY ID LIMIT 1");
   if (mysqli_num_rows($Result) > 0) {
     $RS = mysqli_fetch_assoc($Result);
-    shell_exec("/usr/bin/mpg123 /var/www/html/voice_prompts/" . $RS["filename"] . " &");
+    shell_exec("/usr/bin/mpg123 /var/www/html/voice_prompts/" . $RS["filename"]);
     $Result = mysqli_query($DBcnx,"UPDATE voice_prompts SET seen_by=CONCAT('localhost|',seen_by) WHERE ID=" . $RS["ID"]);
   }
 }
