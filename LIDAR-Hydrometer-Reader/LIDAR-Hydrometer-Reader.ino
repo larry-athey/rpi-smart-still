016a948e96d1afcf57977d10e835b52f4970dcec
Larry Athey <panhandleponics@larryathey.com>
2024-05-04 16:34:36 -0600

    Update LIDAR-Hydrometer-Reader.ino

--- LIDAR-Hydrometer-Reader/LIDAR-Hydrometer-Reader.ino
+++ LIDAR-Hydrometer-Reader/LIDAR-Hydrometer-Reader.ino
@@ -1,7 +1,17 @@
 //------------------------------------------------------------------------------------------------
 // Written by Larry Athey (https://panhandleponics.com) v1.0.1 released July 1, 2024
 //
-
+// If you have seen any of George Duncan's videos on YouTube from around 2020, you might remember
+// an Arduino project of his called the "Talking Parrot Head". It involved a black PVC tube on top
+// of a parrot with a VL53L0X sensor at the top that bounced the beam downward to a disc on top of
+// a glass hydrometer. Depending on the distance, it would play WAV files on an SD card and speak
+// the current ethanol ABV. Kind of bulky and made the parrot top heavy, but it certainly worked.
+//
+// While there was a window in the side near the float point, it still made the hydrometer hard to
+// read visually. Yeah, I know that it was made for a blind guy. Even though I'm 50% blind, I want
+// want to be able to see the hydrometer itself without the obstacle. You can just as easily shoot
+// the beam upward to the bottom side of the disc on the hydrometer and turn that ABV reading into
+// a digital signal that the RPi Smart Still Controller can use.
 //------------------------------------------------------------------------------------------------
 #include "Adafruit_VL53L0X.h"  // VL53L0X LIDAR sensor library by Adafruit
 #include "OneWire.h"           // OneWire Network communications library
@@ -38,22 +48,25 @@ void setup() {
   pinMode(USER_LED,OUTPUT);
 }
 //------------------------------------------------------------------------------------------------
-void TempUpdate() { // Update the distillate temperature
+void TempUpdate() { // Update the distillate temperature value
   DT.requestTemperatures();
   TempC = DT.getTempCByIndex(0);
 }
 //------------------------------------------------------------------------------------------------
-byte CalcEthanol() { // Convert the Distance millimeters to ethanol ABV
+byte CalcEthanol() { // Convert the Distance millimeters to ethanol ABV value
   for (byte x = 0; x <= 10; x ++) {
     if (Divisions[x] == Distance) {
       return x * 10;
     } else {
+      if ((x < 10) && (Distance > Divisions[x]) && (Distance < Divisions[x + 1])) {
 
+      }
     }
   }
+  return 0;
 }
 //------------------------------------------------------------------------------------------------
-void RebootUnit() { // Reboot the device, write to flash memory here before restarting
+void RebootUnit() { // Reboot the device, write to flash memory here before restarting if needed
   ESP.restart();
 }
 //------------------------------------------------------------------------------------------------
