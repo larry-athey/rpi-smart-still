cb91629eacf807f1715093f1f317ffd776dbc999
Larry Athey <panhandleponics@larryathey.com>
2024-09-25 23:14:45 -0600

    Update play-prompts.php

--- Undercarriage/play-prompts.php
+++ Undercarriage/play-prompts.php
@@ -18,6 +18,8 @@ if ($Settings["speech_enabled"] == 1) {
   $Result = mysqli_query($DBcnx,"SELECT * FROM voice_prompts WHERE seen_by NOT LIKE '%localhost%' ORDER BY ID LIMIT 1");
   if (mysqli_num_rows($Result) > 0) {
     $RS = mysqli_fetch_assoc($Result);
+    // NOTE: This will not play the voice prompts if LXDE is active because it hijacks the audio subsystem.
+    //       You will need to enable the Autoplay setting in Firefox if you want to have voice prompts.
     shell_exec("/usr/bin/mpg123 /var/www/html/voice_prompts/" . $RS["filename"]);
     $Result = mysqli_query($DBcnx,"UPDATE voice_prompts SET seen_by=CONCAT('localhost|',seen_by) WHERE ID=" . $RS["ID"]);
   }
