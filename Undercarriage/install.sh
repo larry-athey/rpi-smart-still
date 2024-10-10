e46a5ab7b272b7df61b4567997210323972f7aba
Larry Athey <121518798+larry-athey@users.noreply.github.com>
2024-10-10 13:59:55 -0600

    Update install.sh

--- Undercarriage/install.sh
+++ Undercarriage/install.sh
@@ -36,6 +36,7 @@ fi
 
 sudo dpkg-reconfigure locales
 
+sudo systemctl mask sleep.target suspend.target hibernate.target hybrid-sleep.target
 sudo apt update
 sudo apt install -y alsa-utils espeak ffmpeg mpg123 lighttpd python3 python3-pip python3-dev php php-common php-fpm php-mysql mariadb-server mariadb-client
 sudo apt --fix-broken install -y
