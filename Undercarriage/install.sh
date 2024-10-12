5242d3de573e37ed4e77424c668a908730d013b6
Larry Athey <121518798+larry-athey@users.noreply.github.com>
2024-10-12 12:30:31 -0600

    Update install.sh

--- Undercarriage/install.sh
+++ Undercarriage/install.sh
@@ -169,7 +169,7 @@ if [ $Raspbian -eq 0 ]; then
   if [ ! $? -eq 0 ]; then
     echo "w1-therm" | sudo tee -a /etc/modules
   fi
-  Comfig="/boot/armbianEnv.txt"
+  Config="/boot/armbianEnv.txt"
   if [ -f $Config ]; then
     cat $Config | grep "overlays=w1-gpio"
     if [ ! $? -eq 0 ]; then
