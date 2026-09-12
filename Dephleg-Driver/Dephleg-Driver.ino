d44ffb112f4fb3da67071707a27741f88370ff2d
Larry Athey <panhandleponics@larryathey.com>
2026-09-12 10:15:41 -0600

    Update Dephleg-Driver.ino

--- Dephleg-Driver/Dephleg-Driver.ino
+++ Dephleg-Driver/Dephleg-Driver.ino
@@ -132,9 +132,11 @@ void loop() {
   // Drive PWM (0-255 for 8-bit; change map() if you raised resolution)
   analogWrite(PIN_PWM, map(duty, 0, 100, 0, 255));
 
-  // Zero the accumulators if the motor is completely off
-  if (duty == 0) {
+  if (duty == 0) { // Zero both accumulators if the motor is fully off
     openAccum = 0;
+    closeAccum = 0;
+  } else if (duty == 100) { // Zero the closed accumlator if the motor is fully on
+    openAccum = 10000;
     closeAccum = 0;
   }
 
