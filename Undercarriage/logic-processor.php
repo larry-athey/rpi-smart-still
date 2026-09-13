999853a1d690288f66e42788ea6348cef2058440
Larry Athey <panhandleponics@larryathey.com>
2026-09-13 12:51:01 -0600

    Update logic-processor.php

--- Undercarriage/logic-processor.php
+++ Undercarriage/logic-processor.php
@@ -169,19 +169,23 @@ if (mysqli_num_rows($Result) > 0) {
         // Open to 100% and pull down to the setting to evacuate any air in its water lines
         shell_exec("/usr/share/rpi-smart-still/valve 1 open");
         sleep(10); // Wait for read-inputs.php to detect the limit switch
-        $Duration = $Settings["valve1_total"] - round($Program["condenser_rate"] * $Settings["valve1_pulse"],0);
+        $Program["condenser_rate"] = $Program["condenser_rate"] / 100;
+        $NewPos = round($Settings["valve1_total"] * $Program["condenser_rate"]);
+        $Duration = $Settings["valve1_total"] - $NewPos;
         $Insert = mysqli_query($DBcnx,"INSERT INTO output_table (timestamp,auto_manual,valve_id,direction,duration,position,muted,executed) " .
-                                      "VALUES (now(),'0','1','0','$Duration','" . round($Program["condenser_rate"] * $Settings["valve1_pulse"],0,PHP_ROUND_HALF_UP) . "','1','0')");
-        $Update = mysqli_query($DBcnx,"UPDATE settings SET valve1_position='" . round($Program["condenser_rate"] * $Settings["valve1_pulse"],0,PHP_ROUND_HALF_UP) . "' WHERE ID=1");
+                                      "VALUES (now(),'0','1','0','$Duration','$NewPos','1','0')");
+        $Update = mysqli_query($DBcnx,"UPDATE settings SET valve1_position='$NewPos' WHERE ID=1");
         if ($Program["mode"] == 1) {
           // Open the dephleg valve to its starting position if this is a reflux program
           // Open to 100% and pull down to the setting to evacuate any air in its water lines
           shell_exec("/usr/share/rpi-smart-still/valve 2 open");
           sleep(10); // Wait for read-inputs.php to detect the limit switch
-          $Duration = $Settings["valve2_total"] - round($Program["dephleg_start"] * $Settings["valve2_pulse"],0);
+          $Program["dephleg_start"] = $Program["dephleg_start"] / 100;
+          $NewPos = round($Settings["valve2_total"] * $Program["dephleg_start"]);
+          $Duration = $Settings["valve2_total"] - $NewPos;
           $Insert = mysqli_query($DBcnx,"INSERT INTO output_table (timestamp,auto_manual,valve_id,direction,duration,position,muted,executed) " .
-                                        "VALUES (now(),'0','2','0','$Duration','" . round($Program["dephleg_start"] * $Settings["valve2_pulse"],0,PHP_ROUND_HALF_UP) . "','1','0')");
-          $Update = mysqli_query($DBcnx,"UPDATE settings SET valve2_position='" . round($Program["dephleg_start"]  * $Settings["valve2_pulse"],0,PHP_ROUND_HALF_UP) . "' WHERE ID=1");
+                                        "VALUES (now(),'0','2','0','$Duration','$NewPos','1','0')");
+          $Update = mysqli_query($DBcnx,"UPDATE settings SET valve2_position='$NewPos' WHERE ID=1");
         }
         if ($Settings["hydro_type"] == 0) {
           // Recalibrate the load cell hydrometer to its reference weight
